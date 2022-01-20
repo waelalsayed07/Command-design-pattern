4f241cadc16a2f746151afcc0c1fc91595157e44
waelalsayed07 <egypost43@gmail.com>
2022-01-20 21:52:44 +0200

    Add project files.

---
+++ Command design pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Command_design_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // The client code can parameterize an invoker with any commands.
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+            invoker.DoSomethingImportant();
+        }
+    }
+}
---
+++ Command design pattern/Receiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_design_pattern
+{
+  public  class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+}
